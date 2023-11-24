ac6849c99aa3d611e3cef5b1dcaa3e04833aaa65
Lohann Kasper <lohann.kasper@me.com>
2023-11-24 15:30:37 +0100

    ok

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ define('BASE_URL', '/');
 define('DB_USER', 'todolist');
 define('DB_PASS', 'test123');
 define('DB_NAME', 'todolist');
-define('DB_HOST', '127.0.0.1');
+define('DB_HOST', '20.123.130.131');
 define('DB_PORT', '3306');
 
 $db = new PDO('mysql:host='.DB_HOST.';port='.DB_PORT.';dbname='.DB_NAME, DB_USER, DB_PASS);
